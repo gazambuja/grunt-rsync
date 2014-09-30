aaa6850805339194db1cf691424224e972cd5e34
Gustavo Azambuja <hola@gazambuja.com>
2014-09-30 08:51:35 -0300

    Update rsync.js

--- tasks/rsync.js
+++ tasks/rsync.js
@@ -20,9 +20,10 @@ module.exports = function (grunt) {
     }
     args.push(src.join(' '));
 
+    args.push( "-e 'ssh -p " + options.port + "'" );
+    
     // destination to copy
     args.push((options.user === '' ? '' :  options.user + '@') + options.host + ':' + options.remoteBase + '/' + target); // TODO: normalize
-    args.push( "--rsh='ssh -p" + options.port + "'" );
     
     if(options.deleteAfter && !options.dry){
       args.push('&& rm -rf ' + src.join(' '));
